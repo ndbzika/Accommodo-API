6210c2ef9acabee9b88baaa5c331ec6da842765c
Flavio Henrique <chakcpps@gmail.com>
2023-05-09 22:00:18 -0300

    Criando enums

---
+++ src/main/java/com/hotel/enums/StatusQuarto.java
@@ -0,0 +1,7 @@
+package src.main.java.com.hotel.enums;
+
+public enum StatusQuarto {
+    DISPONIVEL,
+    OCUPADO,
+    MANUTENCAO
+}
---
+++ src/main/java/com/hotel/enums/TiposQuarto.java
@@ -0,0 +1,10 @@
+package src.main.java.com.hotel.enums;
+
+public enum TiposQuarto {
+    INDIVIDUAL,
+    DUPLO_INDIVIDUAL,
+    DUPLO,
+    CASAL,
+    TRIPLO,
+    QUADRUPLO
+}
