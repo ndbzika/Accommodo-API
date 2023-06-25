6cb49002f3ccd9e6b590c7f82b38eaae8c2c6a7e
ndbzika <chakcpps@gmail.com>
2023-06-25 00:42:42 -0300

    feat: Implementando construtores, getters, setters e sobrescrevendo método toString

--- src/main/java/com/example/Accommodo/entity/Pessoa.java
+++ src/main/java/com/example/Accommodo/entity/Pessoa.java
@@ -1,17 +1,8 @@
 package com.example.Accommodo.entity;
 
-
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @MappedSuperclass
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public abstract class Pessoa {
 
     @Column(name = "nome", nullable = false)
@@ -22,4 +13,44 @@ public abstract class Pessoa {
 
     @Column(name = "telefone", nullable = false)
     protected String telefone;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + this.nome +
+                ", Email: " + this.email +
+                ", Telefone " + this.telefone;
+    }
 }
